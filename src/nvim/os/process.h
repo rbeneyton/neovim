a6536fb0594354c7a95d91b68e5f2e18b5d90535
Richard Beneyton <richard.beneyton@gmail.com>
2021-07-19 23:04:47 +0200

    feat(tmux): inform about "tmux location" of process owning a swap file
    When opening a file already open in write mode by a different editor
    instance, neovim reports its corresponding pid, and its state.
    
    When using tmux multiplexer, finding its "real" location (ie.
    server/session/pane/window where the editor seats) requires calling a
    dedicated shell command (tmux list-sessions/list-panes). This
    operation can be easily done directly by neovim.
    
    If the tmux information of the process is retrieved, we add a new
    section in the error message, such:
            process ID: 10091 (STILL RUNNING)
                  tmux: session `dotter` pane `2(build).1`
    
    (cherry picked from commit 03285bc64ca4b8fe122e9d9e520de19c0121ebb7)
    (cherry picked from commit 4ddfe6b7f0c53c8a4a1da0e21b03eef2332fab42)

--- src/nvim/os/process.h
+++ src/nvim/os/process.h
@@ -9,4 +9,9 @@
 # include "os/process.h.generated.h"
 #endif
 
+#if defined(__linux__)
+// TODO auto generated?
+bool os_proc_tmux_info(int pid, char msg[static 512]);
+#endif
+
 #endif  // NVIM_OS_PROCESS_H
