21fdcfed823f5b53704cf8bf4bfe8d8fc36b824f
marvim <marvim@users.noreply.github.com>
2026-04-05 05:10:39 +0000

    docs: update version.c
    vim-patch:9.2.0255: tests: Test_popup_opacity_vsplit() fails in a wide terminal
    vim-patch:9.2.0260: statusline not redrawn after closing a popup window
    vim-patch:9.2.0261: terminal: redraws are slow
    vim-patch:9.2.0264: Cannot disable kitty keyboard protocol in vim :terminal
    vim-patch:9.2.0268: memory leak in call_oc_method()
    vim-patch:9.2.0269: configure: Link error on Solaris
    vim-patch:f4f175332 translation(ru): updated the Russian man page the xxd
    vim-patch:e4502b603 translation(ru): updated lang/README.ru.txt
    vim-patch:2c976d0de SECURITY.md: clarify the use of AI
    vim-patch:9.2.0279: terminal: out-of-bounds write with overlong CSI argument list
    vim-patch:9d83ca5ca runtime(preproc_indent): Ignore Swapfiles when loading buffers
    vim-patch:9.2.0286: still some unnecessary (int) casts in alloc()
    vim-patch:9.2.0288: libvterm: signed integer overflow parsing long CSI args
    vim-patch:bd8b6c6b0 CI: Bump codecov/codecov-action
    vim-patch:9.2.0294: if_lua: lua interface does not work with lua 5.5
    vim-patch:9.2.0297: libvterm: can improve CSI overflow code

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -60,7 +60,7 @@ char *version_cflags = "Compilation: " NVIM_VERSION_CFLAGS;
 // clang-format off
 static const int vim_versions[] = { 801, 802, 900, 901, 902 };
 
-static const int num_patches[] = { 2331, 3803, 1574, 1612, 121 };
+static const int num_patches[] = { 2331, 3803, 1574, 1612, 131 };
 
 static const int *included_patchsets[] = {
   (const int[]) {  // 801
@@ -3816,13 +3816,17 @@ static const int *included_patchsets[] = {
     // 0
   },
   (const int[]) {  // 902
+    270,
+    // 266-269
+    265,
+    // 248-264
+    247, 246, 245, 244, 243, 242,
+    // 240-241
     239,
     // 238
     237,
     // 236
-    235,
-    // 234
-    233,
+    235, 234, 233,
     // 232
     231, 230,
     // 227-229
@@ -3903,7 +3907,9 @@ static const int *included_patchsets[] = {
     59, 58, 57, 56, 55, 54,
     // 53
     52,
-    // 48-51
+    // 51
+    50,
+    // 48-49
     47, 46,
     // 45
     44,
